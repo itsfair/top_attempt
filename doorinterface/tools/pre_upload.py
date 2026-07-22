124ebc0022430f2e9959ccceaedcb30ee5dae75d
simonreufsteck <simonreufsteck@web.de>
2026-07-23 01:32:42 +0200

    Last with GLM

--- doorinterface/tools/pre_upload.py
+++ doorinterface/tools/pre_upload.py
@@ -17,12 +17,3 @@ except Exception:
 flags = f'-DFW_VERSION=\\"{version}\\"'
 env.Append(CCFLAGS=[flags])
 print(f"[pre_upload] FW_VERSION = {version}")
-
-def fix_otadata(*args, **kwargs):
-    port = env.subst("$UPLOAD_PORT")
-    if not port:
-        return
-    print(f"[post_upload] Erasing otadata to force slot 0 on {port}")
-    env.Execute(f'esptool.py --port "{port}" --baud 460800 erase_region 0xd000 0x2000')
-
-env.AddPostAction("upload", fix_otadata)
